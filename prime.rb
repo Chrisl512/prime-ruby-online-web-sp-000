bd62a2fba7d12b6f5621efe314e0ce09afae2af1
Christopher Luna <chris@illusionlayervfx.com>
2019-03-24 02:29:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
 def prime?(num)
-  Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+  Math.sqrt(num).downto(2).each {|i| return false if num % i == 0}
    true
 end
