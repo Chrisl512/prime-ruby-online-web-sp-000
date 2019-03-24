715fc1a9a4cf0db984b64b788a5d3bde315eb4d4
Christopher Luna <chris@illusionlayervfx.com>
2019-03-24 02:19:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
-          true
+  2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil   
+    end
+    return false
 end
