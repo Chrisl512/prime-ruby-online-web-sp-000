b6615e82bfcf79a732050466ad390cbcdd2c670d
Christopher Luna <chris@illusionlayervfx.com>
2019-03-24 02:53:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,18 @@
 require 'pry'
 
 def prime?(num)
-  Math.sqrt(num).downto(-2).each {|i| return false if num % i == 0}
-   true
+  i = 2
+  while i < num
+    is_divisible = ((num % i) == 0)
+
+    if is_divisible
+
+      return false
+    end
+
+    i += 1
+  end
+
+  
+  true
 end
