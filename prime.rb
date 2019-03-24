48401a0cd9560326908fdfe105741b2893c46c5a
Christopher Luna <chris@illusionlayervfx.com>
2019-03-24 03:04:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 def prime?(num)
   if num <= 1
-    return false end
-
+    return false 
+  end
     max = Math.sqrt(num)
-
     (2..max).any? do |i|
-  if num % i == 0 then return false end
+  if num % i == 0 
+    return false 
+  end
   end
 
   true
